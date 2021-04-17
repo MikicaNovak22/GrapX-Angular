b50f0d1d22f30a425447ead3e9a26ca8d5d85548
MikicaNovakovic <mikica22.mn@gmail.com>
2021-04-17 21:55:13 +0200

    Updated home

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './core/navbar/navbar.component';
+import { HomeComponent } from './core/home/home.component';
+import { CardsComponent } from './cards/cards/cards.component';
+import { CardcommentsComponent } from './cards/cardcomments/cardcomments.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    CardsComponent,
+    CardcommentsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/cards/cards/cards.component.ts
@@ -0,0 +1,74 @@
+import { CardComment } from './../../model/comments';
+import { Cards } from './../../model/cards';
+import { GraphxService } from './../../service/graphx.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cards',
+  templateUrl: './cards.component.html',
+  styleUrls: ['./cards.component.css']
+})
+export class CardsComponent implements OnInit {
+
+  parameters ={
+    page: 1,
+    pageSize: 5
+  }
+
+  comments: CardComment[];
+  clicked = false;
+  cardId: number;
+
+  pageSizes = [5, 10, 20]
+
+  cards: Cards[];
+  numberOfCards: number;
+
+  constructor(private service: GraphxService) { }
+
+  ngOnInit(): void {
+    this.getAllCards()
+  }
+
+  getAllCards(){
+    this.service.getAllCards(this.parameters).subscribe(
+      x => {
+        this.cards = x.results;
+        this.numberOfCards = x.count;
+      }
+    )
+  }
+
+  changePage(nova: number){
+    if( nova >= 1 && nova <= (Math.ceil(this.numberOfCards / this.parameters.pageSize))){
+      this.parameters.page = nova;
+      this.getAllCards()
+    }
+  }
+
+  changePageSize(n: number){
+    this.parameters.pageSize = n;
+    this.getAllCards()
+  }
+
+
+  showComments(id: number){
+    this.cardId = id;
+    this.clicked = true;
+    this.service.getComments(id).subscribe(
+      x=> { this.comments = x.results}
+    )
+  }
+
+  setGrade(newGrade: number, id: number){
+    this.service.getOneCard(id).subscribe(
+      x=>{
+        let updatedCard = x;
+        updatedCard.grade = newGrade;
+        this.service.updateCard(updatedCard).subscribe(
+          elem => { this.getAllCards()}
+        )
+      }
+    )
+  }
+}
